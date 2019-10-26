554d6377d9db21775388063276c87e0ff1be8e45
bswalsh247 <bswaslh19@gmail.com>
2019-10-26 14:50:03 -0600

    Using Python to Access Web Data

---
+++ Access Web Python/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+#https://apps.twitter.com/
+#Create new App and ge the four strngs
+
+def oauth():
+    return {"consumer_key": "bFFhGjrOlFfxkAi7wGFYTfjt0",
+            "consumer_secret": "HEx9RL6ytXjBlsFRApVMVtybqlzRpk0limFS1c4Z8BaYk2VplJ",
+            "token_key": "2238579824-xR0UvdQpvVrgRvROK6mSJSWqngUzdO5nDXWQuur",
+            "token_secret": "X79fmekbUBv3wP5CZPC1SnrVtgdACYpajCP2sQUELrrjT"}
